d8109028d8cc9bad638ff8ab2da778707e7eb8cc
Hyunggi Chang <hyunggi.chang95@gmail.com>
2023-08-17 16:19:25 +0900

    link superpoint/glue to executable

--- superpointglue/include/super_point.h
+++ superpointglue/include/super_point.h
@@ -12,7 +12,7 @@
 #include <NvOnnxParser.h>
 #include <opencv2/opencv.hpp>
 
-#include "3rdparty/tensorrtbuffer/include/buffers.h"
+#include "buffers.h"
 #include "read_config.h"
 
 using tensorrt_common::TensorRTUniquePtr;
