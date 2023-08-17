8e3a2da42ec881915146313e4ca0f3abf49dc78c
Hyunggi Chang <hyunggi.chang95@gmail.com>
2023-08-17 17:26:56 +0900

    some updates

--- src/visodo.cpp
+++ src/visodo.cpp
@@ -25,20 +25,20 @@ THE SOFTWARE.
 */
 
 #include "vo_features.h"
+#include "read_config.h"
+#include "super_glue.h"
+#include "super_point.h"
 
-using namespace cv;
-using namespace std;
 
-#define MAX_FRAME 1000
+#define MAX_FRAME 4541
 #define MIN_NUM_FEAT 2000
 
 // IMP: Change the file directories (4 places) according to where your dataset is saved before running!
 
 double getAbsoluteScale(int frame_id, int sequence_id, double z_cal) {
-
-    string line;
+    std::string line;
     int i = 0;
-    ifstream myfile("/data/00/00.txt");
+    std::ifstream myfile("/data/00/00.txt");
     double x = 0, y = 0, z = 0;
     double x_prev, y_prev, z_prev;
     if (myfile.is_open()) {
@@ -58,21 +58,37 @@ double getAbsoluteScale(int frame_id, int sequence_id, double z_cal) {
         }
         myfile.close();
     } else {
-        cout << "Unable to open file" << endl;
+        std::cout << "Unable to open file" << std::endl;
         return 0;
     }
 
-    return sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev));
+    return std::sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev));
 
 }
 
 
 int main(int argc, char **argv) {
+    const std::string config_path = "../config.yaml";
+    const std::string model_dir = "../superpointglue/weights";
 
-    Mat img_1, img_2;
-    Mat R_f, t_f; //the final rotation and tranlation vectors containing the
+    Configs configs(config_path, model_dir);
 
-    ofstream myfile;
+    std::cout << "Building inference engine......" << std::endl;
+    auto superpoint = std::make_shared<SuperPoint>(configs.superpoint_config);
+    if (!superpoint->build()) {
+        std::cerr << "Error in SuperPoint building engine. Please check your onnx model path." << std::endl;
+        return 0;
+    }
+    auto superglue = std::make_shared<SuperGlue>(configs.superglue_config);
+    if (!superglue->build()) {
+        std::cerr << "Error in SuperGlue building engine. Please check your onnx model path." << std::endl;
+        return 0;
+    }
+
+    cv::Mat img_1, img_2;
+    cv::Mat R_f, t_f; //the final rotation and translation vectors containing the
+
+    std::ofstream myfile;
     myfile.open("results1_1.txt");
 
     double scale = 1.00;
@@ -82,14 +98,14 @@ int main(int argc, char **argv) {
     sprintf(filename2, "/data/00/image_0/%06d.png", 1);
 
     char text[100];
-    int fontFace = FONT_HERSHEY_PLAIN;
+    int fontFace = cv::FONT_HERSHEY_PLAIN;
     double fontScale = 1;
     int thickness = 1;
     cv::Point textOrg(10, 50);
 
     //read the first two frames from the dataset
-    Mat img_1_c = imread(filename1);
-    Mat img_2_c = imread(filename2);
+    cv::Mat img_1_c = cv::imread(filename1);
+    cv::Mat img_2_c = cv::imread(filename2);
 
     if (!img_1_c.data || !img_2_c.data) {
         std::cout << " --(!) Error reading images " << std::endl;
@@ -97,28 +113,31 @@ int main(int argc, char **argv) {
     }
 
     // we work with grayscale images
-    cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
-    cvtColor(img_2_c, img_2, COLOR_BGR2GRAY);
+    cv::cvtColor(img_1_c, img_1, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(img_2_c, img_2, cv::COLOR_BGR2GRAY);
 
     // feature detection, tracking
-    vector<Point2f> points1, points2;        //vectors to store the coordinates of the feature points
-    featureDetection(img_1, points1);        //detect features in img_1
-    vector<uchar> status;
+    std::vector<cv::Point2f> points1, points2;        //vectors to store the coordinates of the feature points
+    Eigen::Matrix<double, 259, Eigen::Dynamic> feature_points1, feature_points2;
+    super_featureDetection(superpoint, img_1, points1, feature_points1);        //detect features in img_1
+    std::vector<uchar> status;
     featureTracking(img_1, img_2, points1, points2, status); //track those features to img_2
 
     //TODO: add a fucntion to load these values directly from KITTI's calib files
     // WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
     double focal = 718.8560;
     cv::Point2d pp(607.1928, 185.2157);
-    //recovering the pose and the essential matrix
-    Mat E, R, t, mask;
-    E = findEssentialMat(points2, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
-    recoverPose(E, points2, points1, R, t, focal, pp, mask);
+    //recovering the pose and the essential cv::Matrix
+    cv::Mat E, R, t, mask;
+    E = cv::findEssentialMat(points2, points1, focal, pp, cv::RANSAC, 0.999, 1.0, mask);
+    cv::recoverPose(E, points2, points1, R, t, focal, pp, mask);
 
-    Mat prevImage = img_2;
-    Mat currImage;
-    vector<Point2f> prevFeatures = points2;
-    vector<Point2f> currFeatures;
+    cv::Mat prevImage = img_2;
+    cv::Mat currImage;
+    std::vector<cv::Point2f> prevFeatures = points2;
+    std::vector<cv::Point2f> currFeatures;
+    Eigen::Matrix<double, 259, Eigen::Dynamic> prev_feature_points = feature_points2;
+    Eigen::Matrix<double, 259, Eigen::Dynamic> curr_feature_points;
 
     char filename[100];
 
@@ -127,24 +146,23 @@ int main(int argc, char **argv) {
 
     clock_t begin = clock();
 
-    namedWindow("Road facing camera", WINDOW_AUTOSIZE);// Create a window for display.
-    namedWindow("Trajectory", WINDOW_AUTOSIZE);// Create a window for display.
+    cv::namedWindow("Road facing camera", cv::WINDOW_AUTOSIZE);// Create a window for display.
+    cv::namedWindow("Trajectory", cv::WINDOW_AUTOSIZE);// Create a window for display.
 
-    Mat traj = Mat::zeros(600, 600, CV_8UC3);
+    cv::Mat traj = cv::Mat::zeros(600, 600, CV_8UC3);
 
     for (int numFrame = 2; numFrame < MAX_FRAME; numFrame++) {
         sprintf(filename, "/data/00/image_0/%06d.png", numFrame);
         //cout << numFrame << endl;
-        Mat currImage_c = imread(filename);
-        cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
-        vector<uchar> status;
+        cv::Mat currImage_c = cv::imread(filename);
+        cv::cvtColor(currImage_c, currImage, cv::COLOR_BGR2GRAY);
+        std::vector<uchar> status;
         featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
 
-        E = findEssentialMat(currFeatures, prevFeatures, focal, pp, RANSAC, 0.999, 1.0, mask);
-        recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
+        E = cv::findEssentialMat(currFeatures, prevFeatures, focal, pp, cv::RANSAC, 0.999, 1.0, mask);
+        cv::recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
 
-        Mat prevPts(2, prevFeatures.size(), CV_64F), currPts(2, currFeatures.size(), CV_64F);
-
+        cv::Mat prevPts(2, prevFeatures.size(), CV_64F), currPts(2, currFeatures.size(), CV_64F);
 
         for (int i = 0; i <
                         prevFeatures.size(); i++) {   //this (x,y) combination makes sense as observed from the source code of triangulatePoints on GitHub
@@ -168,40 +186,42 @@ int main(int argc, char **argv) {
             //cout << "scale below 0.1, or incorrect translation" << endl;
         }
 
-        // lines for printing results
-        // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
+        // lines for printing results (KITTI)
+        myfile << R_f.at<double>(0, 0) << " " << R_f.at<double>(0, 1) << " " << R_f.at<double>(0, 2) << " " << t_f.at<double>(0) << " "
+               << R_f.at<double>(1, 0) << " " << R_f.at<double>(1, 1) << " " << R_f.at<double>(1, 2) << " " << t_f.at<double>(1) << " "
+               << R_f.at<double>(2, 0) << " " << R_f.at<double>(2, 1) << " " << R_f.at<double>(2, 2) << " " << t_f.at<double>(2) << std::endl;
 
         // a redetection is triggered in case the number of feautres being trakced go below a particular threshold
         if (prevFeatures.size() < MIN_NUM_FEAT) {
             //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
             //cout << "trigerring redection" << endl;
-            featureDetection(prevImage, prevFeatures);
+            super_featureDetection(superpoint, prevImage, prevFeatures, prev_feature_points);
             featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
 
         }
 
         prevImage = currImage.clone();
         prevFeatures = currFeatures;
+        prev_feature_points = curr_feature_points;
 
         int x = int(t_f.at<double>(0)) + 300;
         int y = int(t_f.at<double>(2)) + 100;
-        circle(traj, Point(x, y), 1, CV_RGB(255, 0, 0), 2);
+        cv::circle(traj, cv::Point(x, y), 1, CV_RGB(255, 0, 0), 2);
 
-        rectangle(traj, Point(10, 30), Point(550, 50), CV_RGB(0, 0, 0), cv::FILLED);
+        cv::rectangle(traj, cv::Point(10, 30), cv::Point(550, 50), CV_RGB(0, 0, 0), cv::FILLED);
         sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm", t_f.at<double>(0), t_f.at<double>(1),
                 t_f.at<double>(2));
-        putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness, 8);
+        putText(traj, text, textOrg, fontFace, fontScale, cv::Scalar::all(255), thickness, 8);
 
-        imshow("Road facing camera", currImage_c);
-        imshow("Trajectory", traj);
+        cv::imshow("Road facing camera", currImage_c);
+        cv::imshow("Trajectory", traj);
 
-        waitKey(1);
-
+        cv::waitKey(1);
     }
 
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout << "Total time taken: " << elapsed_secs << "s" << endl;
+    std::cout << "Total time taken: " << elapsed_secs << "s" << std::endl;
 
     //cout << R_f << endl;
     //cout << t_f << endl;
--- src/vo_features.h
+++ src/vo_features.h
@@ -24,6 +24,10 @@ THE SOFTWARE.
 
 */
 
+#include <memory>
+#include "super_glue.h"
+#include "super_point.h"
+
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -41,41 +45,56 @@ THE SOFTWARE.
 #include <fstream>
 #include <string>
 
-using namespace cv;
-using namespace std;
+void super_featureTracking(std::shared_ptr<SuperGlue> &superglue,
+                           Eigen::Matrix<double, 259, Eigen::Dynamic> &feature_points0,
+                           Eigen::Matrix<double, 259, Eigen::Dynamic> &feature_points1, std::vector<cv::Point2f> &points1,
+                           std::vector<cv::Point2f> &points2, std::vector<uchar> &status) {
+    std::vector<cv::DMatch> superglue_matches;
+    superglue->matching_points(feature_points0, feature_points1, superglue_matches);
+}
 
-void featureTracking(Mat img_1, Mat img_2, vector<Point2f>& points1, vector<Point2f>& points2, vector<uchar>& status)	{ 
+void featureTracking(cv::Mat img_1, cv::Mat img_2, std::vector<cv::Point2f> &points1, std::vector<cv::Point2f> &points2, std::vector<uchar> &status) {
 
 //this function automatically gets rid of points for which tracking fails
 
-  vector<float> err;					
-  Size winSize=Size(21,21);																								
-  TermCriteria termcrit=TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01);
+    std::vector<float> err;
+    const auto winSize = cv::Size(21, 21);
+    const auto termcrit = cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 0.01);
 
-  calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
+    calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
 
-  //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
-  int indexCorrection = 0;
-  for( int i=0; i<status.size(); i++)
-     {  Point2f pt = points2.at(i- indexCorrection);
-     	if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0))	{
-     		  if((pt.x<0)||(pt.y<0))	{
-     		  	status.at(i) = 0;
-     		  }
-     		  points1.erase (points1.begin() + (i - indexCorrection));
-     		  points2.erase (points2.begin() + (i - indexCorrection));
-     		  indexCorrection++;
-     	}
+    //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
+    int indexCorrection = 0;
+    for (int i = 0; i < status.size(); i++) {
+        const auto pt = points2.at(i - indexCorrection);
+        if ((status.at(i) == 0) || (pt.x < 0) || (pt.y < 0)) {
+            if ((pt.x < 0) || (pt.y < 0)) {
+                status.at(i) = 0;
+            }
+            points1.erase(points1.begin() + (i - indexCorrection));
+            points2.erase(points2.begin() + (i - indexCorrection));
+            indexCorrection++;
+        }
 
-     }
+    }
 
 }
 
+void super_featureDetection(std::shared_ptr<SuperPoint> &superpoint, cv::Mat img, std::vector<cv::Point2f> &points,
+                            Eigen::Matrix<double, 259, Eigen::Dynamic> &feature_points) {
+    superpoint->infer(img, feature_points);
 
-void featureDetection(Mat img_1, vector<Point2f>& points1)	{   //uses FAST as of now, modify parameters as necessary
-  vector<KeyPoint> keypoints_1;
-  int fast_threshold = 20;
-  bool nonmaxSuppression = true;
-  FAST(img_1, keypoints_1, fast_threshold, nonmaxSuppression);
-  KeyPoint::convert(keypoints_1, points1, vector<int>());
+    for (int i = 0; i < feature_points.cols(); ++i) {
+        const auto pt = cv::Point2f(feature_points(1, i), feature_points(2, i));
+        points.push_back(pt);
+    }
+}
+
+void featureDetection(cv::Mat img_1,
+                      std::vector<cv::Point2f> &points1) {   //uses FAST as of now, modify parameters as necessary
+    std::vector<cv::KeyPoint> keypoints_1;
+    int fast_threshold = 20;
+    bool nonmaxSuppression = true;
+    cv::FAST(img_1, keypoints_1, fast_threshold, nonmaxSuppression);
+    cv::KeyPoint::convert(keypoints_1, points1, std::vector<int>());
 }
